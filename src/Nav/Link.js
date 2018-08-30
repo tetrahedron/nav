079c29e1fe50a6a3308bb1ec1f7b106d98e2c9fc
Garet Eliot <self@garet.io>
2018-08-30 08:06:31 -0500

    add `to` and `newTab` props to Link

--- src/Nav/Link.js
+++ src/Nav/Link.js
@@ -1,13 +1,23 @@
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { Stylable } from "@tetrahedron/core";
 
-const Link = styled(Stylable("a"))`
+const A = styled(Stylable("a"))`
   display: inline-flex;
   align-items: center;
   text-decoration: none;
   color: inherit;
 `;
+
+const Link = ({ to, newTab, ...props }) => (
+  <A href={to} target={newTab ? "_blank" : null} {...props} />
+);
+
+Link.propTypes = {
+  to: PropTypes.string,
+  newTab: PropTypes.bool,
+};
 
 Link.defaultProps = {
   padding: "0 10px",
